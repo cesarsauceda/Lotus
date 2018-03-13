fe5da1bf4732271ca209d3f53f1f39982ef936c8
alan <iruga.dmc@gmail.com>
2018-03-12 17:58:00 -0700

    comentario de prueba x2

--- app/src/main/java/com/example/sauceda/lotus/RegistroDePsicologos.java
+++ app/src/main/java/com/example/sauceda/lotus/RegistroDePsicologos.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegistroDePsicologos extends AppCompatActivity {
-//pongance las pilas
+//pongance las pilas X2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
