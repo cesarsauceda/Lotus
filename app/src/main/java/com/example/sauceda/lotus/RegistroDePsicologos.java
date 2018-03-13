c642e9d777ac1cca2cc19e9fd4ef9b8f8b22ac1a
cesar <sauceda8888@gmail.com>
2018-03-12 18:04:21 -0700

    Quite comentario

--- app/src/main/java/com/example/sauceda/lotus/RegistroDePsicologos.java
+++ app/src/main/java/com/example/sauceda/lotus/RegistroDePsicologos.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegistroDePsicologos extends AppCompatActivity {
-//pongance las pilas X2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
